25fbcbf590e7f31c2763fc7ae100d2ff564d8918
404kevnotfound <mewzok7@yahoo.com>
2025-05-29 14:00:38 -0600

    - Add verification to received images in URL

--- Frog-Parts/fp_images.php
+++ Frog-Parts/fp_images.php
@@ -2,10 +2,26 @@
     include('fp_functions.php');
     header('Content-Type: image/png');
 
+    // create array of allowed parts
+    $allColors = ['green', 'red', 'blue'];
+    $allArms = ['armfrog', 'armcrab', 'armdog'];
+    $allLegs = ['legfrog', 'legcrab', 'legdog'];
+
     // retrieve variables
-    $color = $_GET['frogcolor'];
-    $arm = $_GET['frogarm'];
-    $leg = $_GET['frogleg'];
+    $color = $_GET['frogcolor'] ?? 'green';
+    $arm = $_GET['frogarm'] ?? 'frog';
+    $leg = $_GET['frogleg'] ?? 'frog';
+
+    // verify input
+    if(!in_array($color, $allColors)) {
+        $color = 'green';
+    }
+    if(!in_array($arm, $allArms)) {
+        $arm = 'frog';
+    }
+    if(!in_array($leg, $allLegs)) {
+        $leg = 'frog';
+    }
 
     $basefrog = imagecreatefrompng("resources/frogbase{$color}.png");
     imagesavealpha($basefrog, true);

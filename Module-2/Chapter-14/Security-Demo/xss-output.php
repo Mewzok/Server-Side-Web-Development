02b7d0f54a31ae1e4cc76f7af2b410843f1b1ebd
404kevnotfound <mewzok7@yahoo.com>
2025-05-29 09:47:48 -0600

    - Add fake website for testing XSS attack

---
+++ Module-2/Chapter-14/Security-Demo/xss-output.php
@@ -0,0 +1,5 @@
+<?php
+// intentionally not using htmlspecialchars
+    $frogname = $_POST['frogname'];
+    echo "<h1>Your frog's name is: $frogname</h1>"; // enter: <script>alert('popup')</script>
+?>

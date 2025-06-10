e9602d226a2baaef08943f3b7a050c6138df1f65
404kevnotfound <mewzok7@yahoo.com>
2025-06-10 09:55:32 -0600

    - Create basic page layout

---
+++ Final-Project/page.php
@@ -0,0 +1,55 @@
+<?php
+    class Page {
+        // class Page's attributes
+        private $content;
+        private $title = "Historical Apology Generator";
+        private $keywords = "historical apology generator";
+
+        public function __construct($title = "Historical Apology Generator", 
+                $keywords = "historical apology generator") {
+            $this->title = $title;
+            $this->keywords = $keywords;
+        }
+
+        public function __set($name, $value) {
+            $this->$name = $value;
+        }
+
+        public function Display() {
+            echo "<html>\n<head>\n";
+            $this -> DisplayTitle();
+            $this -> DisplayKeywords();
+            $this -> DisplayStyles();
+            echo "</head>\n<body>\n";
+            $this -> DisplayHeader();
+            echo $this->content;
+            $this -> DisplayFooter();
+            echo "</body>\n</html>\n";
+        }
+
+        public function DisplayTitle() {
+            echo "<title>".$this->title."</title>";
+        }
+
+        public function DisplayKeywords() {
+            echo "<meta name='keywords' content='".$this->keywords."'/>";
+        }
+
+        public function DisplayStyles() {
+            ?>
+            <link href="style.css" type="text/css" rel="stylesheet">
+            <?php
+        }
+
+        public function DisplayHeader() {
+            ?>
+            <!-- page header -->
+            <a href="index.php" id="headerLink">
+                <header class="page-header">
+                    <h1 id="pageTitle">Historical Apology Generator</h1>
+                </header>
+            </a>
+            <?php
+        }
+    }
+?>
